71f04dc10c86c4124bb5be9489c85aac03969893
AhmedMoustafaHosni <ahmed.hosni94@eng-st.cu.edu.eg>
2017-02-24 16:48:32 +0200

    mimo

--- Matlab/MIMO/cmn_oct2bin.m
+++ Matlab/MIMO/cmn_oct2bin.m
@@ -1,19 +1,3 @@
-%
-% Copyright 2011-2012 Ben Wojtowicz
-%
-%    This program is free software: you can redistribute it and/or modify
-%    it under the terms of the GNU Affero General Public License as published by
-%    the Free Software Foundation, either version 3 of the License, or
-%    (at your option) any later version.
-%
-%    This program is distributed in the hope that it will be useful,
-%    but WITHOUT ANY WARRANTY; without even the implied warranty of
-%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%    GNU Affero General Public License for more details.
-%
-%    You should have received a copy of the GNU Affero General Public License
-%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-%
 % Function:    cmn_oct2bin
 % Description: Converts an array of octal numbers
 %              to an array of binary arrays
@@ -21,12 +5,9 @@
 %              num_bits - Number of bits per octal
 %                         number
 % Outputs:     array    - Array of binary arrays
-% Spec:        N/A
-% Notes:       None
-% Rev History: Ben Wojtowicz 11/22/2011 Created
-%              Ben Wojtowicz 01/29/2012 Fixed license statement
-%              Ben Wojtowicz 02/19/2012 Fixed mod(num_bits,3)==0 bug
-%
+
+
+
 function [array] = cmn_oct2bin(oct, num_bits)
 [junk, num_oct] = size(oct);
 
