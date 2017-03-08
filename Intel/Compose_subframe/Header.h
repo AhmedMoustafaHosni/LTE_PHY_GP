43c0f2b0032c1837f126c741ebca37fbe4e5b044
khaled34 <engkhaledahmed34@gmail.com>
2017-03-08 22:56:54 +0200

    Compose subfram

---
+++ Intel/Compose_subframe/Header.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <float.h>
+#include <mkl.h>
+#include "mkl_service.h"
+#include "mkl_dfti.h"
+#include <omp.h>
+#include <immintrin.h>
+
+
+
+/* Define the format to printf MKL_LONG values */
+#if !defined(MKL_ILP64)
+#define LI "%li"
+#else
+#define LI "%lli"
+#endif
+
+#define NUM_SYM_SUBFRAME 14
+#define Num_SC_RB 12
+#define DATA_SIZE 14400
+
+
+MKL_Complex8 ** compose_subframe(MKL_Complex8* data, MKL_Complex8* dmrs_1, MKL_Complex8* dmrs_2, int RB);
---
+++ Intel/Compose_subframe/main.cpp
@@ -0,0 +1,30 @@
+#include "Header.h"
+
+
+int main()
+{
+	/*Data come from Transform Precoder*/
+	MKL_Complex8 dmrs_1[1200];
+	MKL_Complex8 dmrs_2[1200];
+	MKL_Complex8 data[1200 * 14];
+	dmrs_1[:].real = 0;
+	dmrs_2[:].real = 0;
+	data[:].real = 1;
+	dmrs_1[:].imag = 0;
+	dmrs_2[:].imag = 0;
+	data[:].imag = 0;
+	int RB = 100;
+
+	/*Call The function*/
+	MKL_Complex8 ** gride_out = NULL;
+	double s_initial = 0, s_elapsed = 0;
+	s_initial = dsecnd();  // sample time
+	gride_out = compose_subframe(data, dmrs_1, dmrs_2, RB);
+	s_elapsed = (dsecnd() - s_initial);
+	printf(" completed task1 with == \n == at %.5f milliseconds == \n\n", (s_elapsed * 1000));
+	FILE * fp;
+	fp = fopen("output.txt", "w");
+	fprintf(fp, "REAL = %f    IMAG = %f\n", gride_out[0:13][0:1200].real, gride_out[0:13][0:1200].imag);
+	fclose(fp);
+	return 0;
+}
