5689c80067187e7e2bb96358feafff63ff45bb1b
Ahmad Nour <Eng.Ahmad.Nour@hotmail.com>
2017-07-20 17:05:55 +0200

    Add files via upload

---
+++ CUDA/Final_Timing/Receiver - siso_channel/descrambler.cuh
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cuda_runtime.h>
+#include <helper_math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "device_launch_parameters.h"
+#include <math.h>
+#include <cuda.h>
+#include <device_functions.h>
+#include <cuda_runtime_api.h>
+
+typedef unsigned char Byte;
+typedef signed char signedByte;
+
+//#define startTimer(); cudaEventCreate(&start); cudaEventCreate(&stop); cudaEventRecord(start, 0);
+//#define stopTimer(msg, var); cudaEventRecord(stop, 0); cudaEventSynchronize(stop); cudaEventElapsedTime(&elapsed, start, stop); printf(msg,var);
+#define destroyTimers(); 	cudaEventDestroy(start); cudaEventDestroy(stop);
+
+//Example for timer macros usage:
+//	startTimer();
+//  ...do_something();
+//  stopTimer("Time= %.10f ms\n");
+//  ...at the very end
+//  destroyTimers();
+
+__global__ void descrabmler(Byte *bits_d, Byte *descrambledbits_d, Byte *c_d, int numThreads);
+void descrambler(Byte* bits_d, Byte** descrambledbits_d, Byte* c_d, const int N);
