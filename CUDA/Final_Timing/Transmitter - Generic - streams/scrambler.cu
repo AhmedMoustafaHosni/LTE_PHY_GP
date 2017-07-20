5bb2eab35fbdf54620193924eeac19319eb0a5e7
Ahmad Nour <Eng.Ahmad.Nour@hotmail.com>
2017-07-20 17:03:29 +0200

    Add files via upload

---
+++ CUDA/Final_Timing/Transmitter - Generic - streams/scrambler.cu
@@ -0,0 +1,34 @@
+/*
+% Function:		scrambler
+% Description:	scramble bits with psuedo random seq.
+% Inputs:		bits_h:				Binary bits to scramble
+%				c_h:					psuedo random sequence
+% Outputs:		*scrambledbits_h:	Scrambled Bits
+By: Ahmad Nour & Mohammed Mostafa
+*/
+
+#include "scrambler.cuh"
+
+__global__ void scrabmler(Byte *bits_d, Byte *scrambledbits_d, Byte *c_d, int numThreads)
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	//Not to run more threads than available data
+	if (idx >= numThreads)
+		return;
+
+	scrambledbits_d[idx] = bits_d[idx] ^ c_d[idx];
+}
+
+void scrambler(Byte* bits_d, Byte** scrambledbits_d, Byte* c_d, const int N, cudaStream_t stream)
+{
+
+	//Calc. number of needed threads for calling kernel(s)
+	int numThreads = N;
+	int blockDim = (numThreads < 1024) ? numThreads : 1024;	//block size in threads (max 1024 thread)
+	int gridDim = numThreads / (blockDim)+(numThreads % blockDim == 0 ? 0 : 1); //grid size in bloack (min 1)
+
+	//Calling the kernel(s)
+	scrabmler << <gridDim, blockDim, 0, stream >> > (bits_d, *scrambledbits_d, c_d, N);
+
+}
---
+++ CUDA/Final_Timing/Transmitter - Generic - streams/transform_precoder.cu
@@ -0,0 +1,46 @@
+/*
+% Function:		transform_precoder
+% Description:	perform transform precoding on complex data after mapper
+% Inputs:		*symbols_R_h:	Real part of the symbols
+% Inputs:		*symbols_I_h:	Imag part of the symbols
+%				M_pusch_rb		numer of resource blocks assigned to ue
+% Outputs:		*precoded_data	transform precodded data
+By: Ahmad Nour & Mohammed Mostafa
+*/
+
+/*
+toComplex kernel converts from 2-array implementation 2 cufftComplex structure and
+multiples symbols by a coeff (1/sqrt(M_pusch_sc)). The kernel's overhead can be avoided if we
+merged it with the mapper kernel
+*/
+
+#include "transform_precoder.cuh"
+
+__global__ void toComplex(float* symbols_R_d, float* symbols_I_d, cufftComplex* symbols_d, double coeff, int numThreads) {
+
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	//Not to run more threads than available data
+	if (idx >= numThreads)
+		return;
+
+	symbols_d[idx].x = symbols_R_d[idx] * coeff;
+	symbols_d[idx].y = symbols_I_d[idx] * coeff;
+
+}
+
+void transform_precoder(float* symbols_R_d,float* symbols_I_d, const int M_pusch_rb, int signal_size, cufftComplex** precoded_data_d, cufftHandle plan_transform_precoder, cufftComplex* symbols_d, cudaStream_t stream)
+{
+	int M_pusch_sc = N_sc_rb * M_pusch_rb;
+
+	//Calc. number of needed threads for calling kernel(s)
+	int numThreads = signal_size;
+	int blockDim = (numThreads < 1024) ? numThreads : 1024;	//block size in threads (max 1024 thread)
+	int gridDim = numThreads / (blockDim)+(numThreads % blockDim == 0 ? 0 : 1); //grid size in bloack (min 1)
+
+	//Coeff. Multiplication
+	toComplex << <gridDim, blockDim  ,0, stream >> > (symbols_R_d, symbols_I_d, symbols_d, rsqrtf(M_pusch_sc), numThreads);
+	cufftSetStream(plan_transform_precoder, stream);
+	cufftExecC2C(plan_transform_precoder, symbols_d, *precoded_data_d, CUFFT_FORWARD);
+
+}
